80ff12c9e46ffc34d11ee04142400e4b65d3a159
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-17 19:09:29 +0600

    all user updated

--- controller/allUsers.js
+++ controller/allUsers.js
@@ -1,9 +1,16 @@
+const userModel = require("../models/userModel")
+
 async function allUsers(req,res){
     try {
         console.log("userId all user",req.userId)
 
+        const allUsers = await userModel.find()
+
         res.json({
-            message : "All user"
+            message : "All user",
+            data : allUsers,
+            success : true,
+            error : false
         })
     } catch (err) {
         res.status(400).json({
