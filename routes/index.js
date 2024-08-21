b8b499549c66c6dd8990f0949eabb97a0cff2335
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-21 21:48:46 +0600

    update user section added

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const authToken = require('../middleware/authToken')
 const userDetailsController = require('../controller/userDetails')
 const userLogout = require('../controller/userLogout')
 const allUsers = require('../controller/allUsers')
+const updateUser = require('../controller/updateUser')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -16,5 +17,6 @@ router.get("/userLogout",userLogout)
 
 // admin panel router dom
 router.get("/all-user",authToken, allUsers)
+router.post("/update-user",authToken,updateUser)
 
 module.exports = router
