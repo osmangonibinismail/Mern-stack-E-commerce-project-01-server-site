5cd55330053cf05bade9076b94790c8cad26deca
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-24 05:55:27 +0600

    uploadProduct page added

---
+++ controller/uploadProduct.js
@@ -0,0 +1,12 @@
+
+async function UploadProductController(req,res){
+    try {
+        
+    } catch (err) {
+        res.status(400).json({
+            message: err.message || err,
+            error: true,
+            success: false
+        })
+    }
+}
