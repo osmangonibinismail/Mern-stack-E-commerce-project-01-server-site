7647b32615fea78445c0fdc0ea89f33251ad667e
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-24 06:11:37 +0600

    product model page section added and upload model section updated

--- controller/uploadProduct.js
+++ controller/uploadProduct.js
@@ -1,7 +1,16 @@
+const productModel = require("../models/productModel")
 
 async function UploadProductController(req,res){
     try {
-        
+        const uploadProduct = new productModel(req.body)
+        const saveProduct = await uploadProduct.save()
+
+        res.status(201).json({
+            message : "Product upload successfully",
+            error: false,
+            success : true,
+            data : saveProduct
+        })
     } catch (err) {
         res.status(400).json({
             message: err.message || err,
@@ -10,3 +19,5 @@ async function UploadProductController(req,res){
         })
     }
 }
+
+module.exports = UploadProductController
