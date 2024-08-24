bd93d14f70ae6cf82366939d42b37e3551cbca9a
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-24 06:52:48 +0600

    permission.js page added

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const userDetailsController = require('../controller/userDetails')
 const userLogout = require('../controller/userLogout')
 const allUsers = require('../controller/allUsers')
 const updateUser = require('../controller/updateUser')
+const UploadProductController = require('../controller/uploadProduct')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -18,5 +19,8 @@ router.get("/userLogout",userLogout)
 // admin panel router dom
 router.get("/all-user",authToken, allUsers)
 router.post("/update-user",authToken,updateUser)
+
+// product
+router.post("/upload-product",authToken,UploadProductController)
 
 module.exports = router
