d7cd73cc43894e1b5323d85d895db5af1f9f02d6
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-26 05:28:11 +0600

    get category product file added

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ const updateUser = require('../controller/user/updateUser')
 const UploadProductController = require('../controller/product/uploadProduct')
 const getProductController = require('../controller/product/getProduct')
 const updateProductController = require('../controller/product/updateProduct')
+const getCategoryProduct = require('../controller/product/getCategoryProduct')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -26,5 +27,6 @@ router.post("/update-user",authToken,updateUser)
 router.post("/upload-product",authToken,UploadProductController)
 router.get("/get-product",getProductController)
 router.post("/update-product",authToken,updateProductController)
+router.get("/get-categoryProduct",getCategoryProduct)
 
 module.exports = router
