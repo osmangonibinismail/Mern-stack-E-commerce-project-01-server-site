b0b2b0ecbe3cc88e869e9584ccd369740eaba0cd
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-27 06:30:56 +0600

    get category wise product section added

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ const UploadProductController = require('../controller/product/uploadProduct')
 const getProductController = require('../controller/product/getProduct')
 const updateProductController = require('../controller/product/updateProduct')
 const getCategoryProduct = require('../controller/product/getCategoryProductOne')
+const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -28,5 +29,6 @@ router.post("/upload-product",authToken,UploadProductController)
 router.get("/get-product",getProductController)
 router.post("/update-product",authToken,updateProductController)
 router.get("/get-categoryProduct",getCategoryProduct)
+router.post("/category-product",getCategoryWiseProduct)
 
 module.exports = router
