3a30f046b6989591c4f81d7fac2ccee8377f00d0
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-01 00:24:02 +0600

    count add to cart product added

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ const getCategoryProduct = require('../controller/product/getCategoryProductOne'
 const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct')
 const getProductDetails = require('../controller/product/getProductDetails')
 const addToCartController = require('../controller/user/addToCartController')
+const countAddToCartProduct = require('../controller/user/countAddToCartProduct')
 
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
@@ -36,5 +37,7 @@ router.post("/product-details",getProductDetails)
 
 // user add to cart
 router.post("/addToCart",authToken,addToCartController)
+router.get("/countAddToCartProduct",authToken,countAddToCartProduct)
+
 
 module.exports = router
