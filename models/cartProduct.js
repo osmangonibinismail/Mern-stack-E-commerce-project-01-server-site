0abb1dc274d2c6d479c3c58a1627c021cd62bf30
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-09-01 05:32:24 +0600

    add to cart view product section added

--- models/cartProduct.js
+++ models/cartProduct.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 
 const addToCart = mongoose.Schema({
-    productId : String,
-    quantity : Number,
-    userId : String,
-},{
-    timestamps : true
+    productId: {
+        ref: 'product',
+        type: String,
+    },
+    quantity: Number,
+    userId: String,
+}, {
+    timestamps: true
 })
 
-const addToCartModel = mongoose.model("addToCart",addToCart)
+const addToCartModel = mongoose.model("addToCart", addToCart)
 module.exports = addToCartModel
